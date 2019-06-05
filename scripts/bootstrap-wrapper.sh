87d08629a5f42ff76c77e461d8621d99a3e76823
Lukáš Staněk <ls@elostech.cz>
2019-06-05 20:37:05 +0200

    Uprava predavania parametrov

--- scripts/bootstrap-wrapper.sh
+++ scripts/bootstrap-wrapper.sh
@@ -5,5 +5,5 @@ git clone https://github.com/elos-tech/kubernetes-cicd.git
 cd kubernetes-cicd
 #./scripts/bootstrap.sh "$@"
 
-echo "./scripts/bootstrap.sh $@" > /deploy.sh
+echo ./scripts/bootstrap.sh "$@" > /deploy.sh
 chmod +x /deploy.sh
