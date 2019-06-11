6e39fd2c79bac1158c374c758d2ec5b9ba079b98
Lukáš Staněk <ls@elostech.cz>
2019-06-11 16:53:08 +0200

    Oprava outputu

--- scripts/bootstrap-wrapper.sh
+++ scripts/bootstrap-wrapper.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-cd /root
-git clone https://github.com/elos-tech/kubernetes-cicd.git
-cd kubernetes-cicd
+{
+  cd /root
+  git clone https://github.com/elos-tech/kubernetes-cicd.git 
+  cd kubernetes-cicd
 
-# Just for troubleshooting.
-echo ./scripts/bootstrap.sh "$@" > /deploy.sh
-chmod +x /deploy.sh
+  # Just for troubleshooting.
+  echo ./scripts/bootstrap.sh "$@" > /deploy.sh
+  chmod +x /deploy.sh
+
+  echo EMPTY > /http_application_routing_zone
+} &> /dev/null
 
 sudo -u root ./scripts/bootstrap.sh "$@" &> /deployment.log
+
+# Stdout should go to ARM deployment output. This is the only output
+# of this script!
+cat /http_application_routing_zone
