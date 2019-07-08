7c052113f77fe862e451d37ddd1d19ca1a01a5cf
Lukas Stanek <ls@elostech.cz>
2019-07-08 13:29:52 +0200

    Fix skriptu

--- scripts/aks/create_marketplace_package.sh
+++ scripts/aks/create_marketplace_package.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ ! -d 'azure' ]; then
+  echo 'Error: Script should be executed from root directory of kubernetes-cicd repo!'
+  echo 'Proper usage: ./scripts/aks/create_marketplace_package.sh'
+  exit 1
+fi
+
 TMPDIR=$(mktemp -d)
 ZIPFILE='/tmp/cicd-app-marketplace.zip'
 
